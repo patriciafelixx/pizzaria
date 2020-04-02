43c3244ed1751d77562ffa7efbd9263f00755fdd
Patricia Felix <patriciafelixx@outlook.com>
2020-04-02 17:20:53 -0300

    Rotas e Controllers

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,13 +1,114 @@
 const pizzas = require('../database/Pizzas.json');
+const fs = require('fs');
 
 module.exports = {
-    index: (req, res) => {
-    //  res.send('Ta tudo indo bem...')
-        res.render('index', { pizzas });
-    },
-    show: (req, res) => {
-        // retornando a pizza com o id passado na rota
-        let pizza = pizzas.find( pizza => pizza.id == req.params.id);
-        return pizza ? res.render('pizza', { pizza }) : res.render('erros/pizzaNaoEncontrada', { id: req.params.id });
-    }
+	index: (req, res)=>{
+		res.render("index",{pizzas,busca:""});
+	},
+	show: (req, res) => {
+
+		// Capturando a pizza com o id passado na rota
+		let pizza = pizzas.find(
+			pizza => pizza.id == req.params.id
+		);
+
+		// Capturando a posição da pizza no array
+		let pos = pizzas.indexOf(pizza);
+
+		// determinando o id da próxima pizza e da anterior
+		let idPrev = null;
+		let idNext = null;
+
+		if(pos > 0){
+			idPrev = pizzas[pos -1].id;
+		}
+
+		if(pos < pizzas.length - 1){
+			idNext = pizzas[pos + 1].id;
+		}
+
+		// Retornando a pizza para o usuário
+		if(pizza){
+			res.render("pizza",{pizza, idNext, idPrev});
+		} else {
+			res.render("erros/pizzaNaoEncontrada",{id:req.params.id});
+		}
+	},
+	search: (req,res) => {
+		let busca = req.query.q;
+		if(busca){
+			let result = pizzas.filter( p => p.nome.toUpperCase().includes(busca.toUpperCase()) );
+			return res.render('index', {pizzas: result, busca});
+		} else {
+			return res.redirect('/');
+		}
+	},
+	edit: (req, res) =>{
+		// Carregar a pizza de id passado pela rota
+		let pizza = pizzas.find(
+			(pizza) => {
+				return pizza.id == req.params.id;
+			}
+		)
+
+		// Enviar view edit-pizza passando para ela a pizza carregada
+		return res.render("crud-pizzas/edit",{pizza});
+	},
+	create: (req,res) => {
+		return res.render("crud-pizzas/create");
+	},
+	list: (req,res) => {
+		return res.render("crud-pizzas/list",{pizzas});
+	},
+	store: (req, res) => {
+		
+		// Capturar as info enviadas pelo usuário
+		let { nome, ingredientes, preco } = req.body;
+		ingredientes = ingredientes.split(',');
+		ingredientes = ingredientes.map(ing => ing.trim());
+		
+		// Tratar o upload do arquivo
+
+		// Criar um objeto literal representando uma pizza com as info enviadas
+		let id = pizzas[pizzas.length - 1].id + 1;
+		const pizza = { id, nome, ingredientes, preco: Number(preco), destaque: false, img: ''};
+		
+		// Adicionar a pizza criada ao array de pizzas
+		pizzas.push(pizza);
+		
+		// Salvar o array de pizzas no Pizzas.json
+		fs.writeFileSync('database/Pizzas.json', JSON.stringify(pizzas));
+
+		// Redirecionar o usuário para a lista de pizzas
+		res.redirect('/pizzas')
+	},
+	update: (req, res) => {
+		// Capturar o id da pizza a ser alterada
+		const { id } = req.params;
+
+		// Capturar os novos dados da pizza
+		const { nome, ingredientes, preco } = req.body;
+
+		//  Alterar os campos (exceto a imagem)
+		// const index = pizzas.indexOf(pizza => pizza.id == id);
+		let pizza = pizzas.find(pizza => pizza.id == id);
+		pizza.nome = nome;
+		pizza.preco = Number(preco);
+		pizza.ingredientes = ingredientes.split(',').map(ing => ing.trim());
+		
+		// Salvar o array de pizzas no arquivo JSON
+		fs.writeFileSync('database/Pizzas.json', JSON.stringify(pizzas));
+
+		// Redirecionar o usuário apara a lista de pizzas
+		res.redirect('/pizzas');		
+	},
+	delete: (req, res) => {
+		const { id } = req.params;
+
+		// Remover a pizza
+		pizzas.splice(pizzas.findIndex(e => e.id == id), 1);
+
+		fs.writeFileSync('database/Pizzas.json', JSON.stringify(pizzas));
+		res.redirect('/pizzas');		
+	}
 }
---
+++ routes/PizzasRouter.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+const PizzasController = require('../controllers/PizzasController');
+
+/* GET home page. */
+router.get('/', PizzasController.index);
+router.get('/busca',PizzasController.search);
+router.get('/pizzas/:id/edit', PizzasController.edit);
+router.get('/pizzas/create', PizzasController.create);
+router.get('/pizzas/:id',PizzasController.show);
+router.get('/pizzas',PizzasController.list);
+router.post('/pizzas',PizzasController.store);
+router.put('/pizzas/:id/update', PizzasController.update);
+router.delete('/pizzas/:id', PizzasController.delete);
+
+module.exports = router;
